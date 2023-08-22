6cc2ebcac81abcf9a00369a6ccd18f1eb235d553
Noah <noah.ntabo@student.moringaschool.com>
2023-08-22 23:34:18 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self,initialValue=""):
+        self._value = initialValue
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, str):
+            print("The value must be a string.")
+        else:
+            self._value = val
+            
+    def is_sentence(self):
+        return self._value.endswith(".")
+    
+    def is_question(self):
+        return self._value.endswith("?")
+    
+    def is_exclamation(self):
+        return self._value.endswith("!")
+    
+    def count_sentences(self):
+        # Split the value using punctuation marks as delimiters
+        pass
+
+    
